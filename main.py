686fd95900657c015332a49c4af31741b52d4666
SaleemLawal <slawal1@umbc.edu>
2023-05-26 10:48:25 -0400

    Implemented the program to decode words to morse code

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from morse_dict import decode
 
+def translate(word: str) -> list:
+    morse_list = []
+    for letter in word:
+        for key, val in decode.items(): 
+            if val == letter: morse_list.append(key)
+    return morse_list
+    
+    
+if __name__ == '__main__':
+    word_to_morse = input("Enter a word to translate: ").upper()
+    morse_code = translate(word_to_morse)
+
+    for i in range(len(morse_code)):
+        print(f"{word_to_morse[i]} is {morse_code[i]}")
